f0bb7a78e41e1c9666918df732bdcdd35867b65e
unknown <muraliagni@outlook.com>
2018-12-19 17:47:53 +0530

    Assingment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutProfileComponent } from './profile/about-profile/about-profile.component';
+import { ContactInfoComponent } from './profile/contact-info/contact-info.component';
+import { EducationComponent } from './education/education.component';
+import { SchoolComponent } from './education/school/school.component';
+import { PortfolioComponent } from './education/portfolio/portfolio.component';
+import { SkillsComponent } from './education/skills/skills.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutProfileComponent,
+    ContactInfoComponent,
+    EducationComponent,
+    SchoolComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    
+    
+   
   ],
   imports: [
     BrowserModule,
